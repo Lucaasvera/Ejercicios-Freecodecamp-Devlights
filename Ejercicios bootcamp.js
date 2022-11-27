dd70ee5b0a02105411278c4990de1699228ed0c1
Lucas Vera <86854922+Lucaasvera@users.noreply.github.com>
2022-11-27 15:25:14 -0300

    Add files via upload

---
+++ Ejercicios bootcamp.js
@@ -0,0 +1,245 @@
+//Ejercicio 1
+
+//Creando comentarios de una linea
+/* Comentarios de
+varias
+ lineas */
+
+//Ejercicio 2 declara una variable
+
+var myName;
+
+//Ejercico 3 asignando un valor a una variable
+
+var a;
+a = 10;
+
+//Ejercicio 4 Asignar el valor de una variable a otra variable
+
+var a;
+a = 7;
+var b;
+b = a;
+
+//Ejercicio 5 Inicializar variables
+
+var a = 9;
+
+//Ejercicio 6 Declarar variables de cadena
+
+var myFirstName = "Lucas";
+var myLastName = "Vera";
+
+//Ejercicio 7 Comprendiendo las variables no inicializadas
+
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+//Ejercicio 8 Comprendiendo la sensibilidad de mayusculas en las variables
+
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+//Ejercicio 9 Diferencias de var y let
+
+let catName = "Oliver";
+let catSound = "Meow!";
+
+//Ejercicio 10 Declarar una variable de solo lectura con const
+
+const FCC = "freeCodeCamp";
+let fact = "is cool!";
+fact = "is awesome!";
+console.log(FCC, fact);
+
+//Ejercicio 11 Suma
+
+const sum = 10 + 10;
+
+//Ejercicio 12 Resta
+
+const difference = 45 - 33;
+
+//Ejercicio 13 Multiplicacion
+
+const product = 8 * 10;
+
+//Ejercicio 14 Dividir
+
+const quotient = 66 / 33;
+
+//Ejercicio 15 Incrementa un numero con JS
+
+let myVar = 87;
+myVar++;
+
+//Ejercicio 16 Decrementa un numero con JS
+
+let myVar1 = 11;
+myVar1--;
+
+//Ejercicio 17 Crea numeros decimales con JS
+
+const ourDecimal = 5.7;
+const myDecimal = 2.3;
+
+//Ejercicio 18 Multiplica dos numeros decimales
+
+const product1 = 2.0 * 2.5;
+
+//Ejercicio 19 Divide dos numeros decimales
+
+const quotient1 = 4.4 / 2.9;
+
+//Ejercicio 20 Encuentra un resto de división
+
+const remainder = 11 % 3;
+
+//Ejercicio 21 Asignacion compuesta con adicion aumentada
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+a += 12;
+b += 9;
+c += 7;
+
+//Ejercicio 22 Asignacion compuesta con resta aumentada
+let a = 11;
+let b = 9;
+let c = 3;
+
+a -= 6;
+b -= 15;
+c -= 1;
+
+//Ejercicio 23 Asignacion compuesta con multiplicacion aumentada
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+a *= 5;
+b *= 3;
+c *= 10;
+
+//Ejercicio 24 Asignacion compuesta con division aumentada
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+a /= 12;
+b /= 4;
+c /= 11;
+
+//Ejercicio 25 Escapa comillas literales en cadenas
+
+const myStr = 'I am a "double quoted" string inside "double quotes".';
+
+//Ejercicio 26 Cita cadenas con comillas simples
+
+const myStr1 = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+//Ejercicio 27 Escapa secuencias en cadenas
+
+const myStr2 = "FirstLine\n\t\\SecondLine\nThirdLine";
+
+//Ejercicio 28 Concatena cadenas con el operador +
+
+const myStr3 = "This is the start." + " This is the end.";
+
+//Ejercicio 29 Concatena cadenas con el operador +=
+
+let myStr4 = "This is the first sentence.";
+myStr4 += " This is the second sentence.";
+
+//Ejercicio 30 Construye cadenas con variables
+
+const myName = "Lucas";
+const myStr5 = "My name is " + myName + " and I am well!";
+
+//Ejercicio 31 Agrega variables a cadenas
+
+const someAdjective = "amazing";
+let myStr6 = "Learning to code is ";
+myStr6 += someAdjective;
+
+//Ejericio 32 Encuentra la longitud de una cadena
+
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+lastNameLength = lastName.length;
+
+//Ejercicio 33 Utiliza la notacion de corchetes
+//para encontrar el primer caracter de una cadena
+let firstLetterOfLastName = "";
+const lastName0 = "Lovelace";
+
+firstLetterOfLastName = lastName0[0];
+
+//Ejercicio 34 Comprende la inmutabilidad de las cadenas
+
+let myStr7 = "Jello World";
+
+myStr7 = "Hello World";
+
+//Ejercicio 35 Utiliza la notacion de corchetes
+//para encontrar el enesimo caracter de una cadena
+
+const lastName1 = "Lovelace";
+
+const thirdLetterOfLastName = lastName1[2];
+
+//Ejercicio 36 Utiliza la notacion de corchetes para
+//encontrar el ultimo caracter de una cadena
+
+const lastName2 = "Lovelace";
+
+const lastLetterOfLastName = lastName2[lastName.length - 1];
+
+//Ejercicio 37 Utiliza la notacion de corchetes para
+//encontrar el caracter enesimo final en una cadena
+
+const lastName3 = "Lovelace";
+const secondToLastLetterOfLastName = lastName3[lastName3.length - 2];
+
+//Ejercicio 38 Palabra en blanco
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+const wordBlanks =
+  "The " +
+  myAdjective +
+  " " +
+  myNoun +
+  " " +
+  myVerb +
+  " " +
+  "very" +
+  " " +
+  myAdverb +
+  ".";
+
+//Ejercicio 39 Almacena multiples valores en una variable utilizando los arreglos de JS
+
+const myArray = ["Lucas", 23];
+
+//Ejercicio 40 anida un arreglo dentro de otro arrelgo
+
+const myArray0 = ["Lucas", 23, ["Vera", "Developer"]];
